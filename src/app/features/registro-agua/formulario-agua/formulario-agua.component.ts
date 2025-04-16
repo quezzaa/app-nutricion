6d7597a38daed2f87bafa318fa757935f7c6e442
quezzaa <quezadita0409@gmail.com>
2025-04-15 20:38:17 -0600

    Formulario Agua

--- src/app/features/registro-agua/formulario-agua/formulario-agua.component.ts
+++ src/app/features/registro-agua/formulario-agua/formulario-agua.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-formulario-agua',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './formulario-agua.component.html',
   styleUrl: './formulario-agua.component.css'
 })
 export class FormularioAguaComponent {
+  agua={
+    cantidad: 0,
+    hora: '',
+  }
+
+  guardarAgua() {
+    const fechaHoy = new Date();
+    const horaConsumida = this.agua.hora;
+    fechaHoy.setHours(parseInt(horaConsumida.split(':')[0]), parseInt(horaConsumida.split(':')[1]), 0, 0);
+    console.log("Agua registrada para: ", fechaHoy);
+    // Aquí iría el guardado en almacenamiento local
+  }
 
 }
