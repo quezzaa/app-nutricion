881295961fbce7882b8664dae81abb32e06c4b66
quezzaa <quezadita0409@gmail.com>
2025-04-15 20:38:24 -0600

    Formulario Perfil

--- src/app/features/perfil-usuario/formulario-perfil/formulario-perfil.component.ts
+++ src/app/features/perfil-usuario/formulario-perfil/formulario-perfil.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-perfil',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './formulario-perfil.component.html',
   styleUrl: './formulario-perfil.component.css'
 })
 export class FormularioPerfilComponent {
+  usuario = {
+    nombre: 'Juan Pérez',
+    email: 'juan@example.com',
+    edad: 25,
+    altura: 175
+  };
 
+  // Método para guardar los cambios del perfil
+  guardarPerfil() {
+    console.log("Perfil actualizado:", this.usuario);
+    // Aquí podrías hacer un llamado a tu backend o almacenamiento para guardar los cambios.
+  }
 }
