ae6ebb6c223db343b9e184b59cc119612ec4100f
quezzaa <quezadita0409@gmail.com>
2025-04-18 11:05:18 -0600

    Terminado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
     provideAnimations(),
     provideToastr(),
+    provideCharts(withDefaultRegisterables())
   ]
 };
--- src/app/features/estadisticas/grafico-diario/grafico-diario.component.ts
+++ src/app/features/estadisticas/grafico-diario/grafico-diario.component.ts
@@ -1,25 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { ComidaServiceService } from '../../../services/comida-service.service';
 import { AguaServiceService } from '../../../services/agua-service.service';
-import { RegistroComida } from '../../../Models/comida.model';
-import { Agua } from '../../../Models/agua.model';
 import { ToastrService } from 'ngx-toastr';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartOptions, ChartData } from 'chart.js';
+
 @Component({
-    selector: 'app-grafico-diario',
-    standalone: true,
-    imports: [],
-    templateUrl: './grafico-diario.component.html',
-    styleUrl: './grafico-diario.component.css'
+  selector: 'app-grafico-diario',
+  standalone: true,
+  imports: [BaseChartDirective],
+  templateUrl: './grafico-diario.component.html',
+  styleUrl: './grafico-diario.component.css'
 })
-export class GraficoDiarioComponent {
-  resumenSemanal = this.resetResumen();
-  resumenMensual = this.resetResumen();
+export class GraficoDiarioComponent implements OnInit {
+
+  public barChartOptionsSemana: ChartOptions = {
+    responsive: true,
+    indexAxis: 'y',
+    scales: {
+      x: {
+        beginAtZero: true
+      }
+    }
+  };
+
+  public barChartLabelsSemana: string[] = [];
+  public barChartDataSemana: ChartData<'bar'> = {
+    labels: this.barChartLabelsSemana,
+    datasets: [
+      { data: [], label: 'Semana' }
+    ]
+  };
+
+  public barChartOptionsMes: ChartOptions = {
+    responsive: true,
+    indexAxis: 'y',
+    scales: {
+      x: {
+        beginAtZero: true
+      }
+    }
+  };
+
+  public barChartLabelsMes: string[] = [];
+  public barChartDataMes: ChartData<'bar'> = {
+    labels: this.barChartLabelsMes,
+    datasets: [
+      { data: [], label: 'Mes' }
+    ]
+  };
 
   constructor(
     private CService: ComidaServiceService,
     private AService: AguaServiceService,
-    private toast:ToastrService
+    private toast: ToastrService
   ) {}
+
   ngOnInit(): void {
     this.calcularEstadisticas();
   }
@@ -43,32 +79,92 @@ export class GraficoDiarioComponent {
     const aguaSemana = await this.AService.obtenerAguaDeLaSemana();
     const aguaMes = await this.AService.obtenerAguaDelMes();
 
-    for (const comida of comidasSemana) {
-      this.resumenSemanal.calorias += comida.totales.calorias;
-      this.resumenSemanal.proteina += comida.totales.proteina;
-      this.resumenSemanal.carbohidratos += comida.totales.carbohidratos;
-      this.resumenSemanal.grasas += comida.totales.grasas;
-      this.resumenSemanal.fibra += comida.totales.fibra;
-      this.resumenSemanal.azucares += comida.totales.azucares;
-      this.resumenSemanal.sodio += comida.totales.sodio;
+    let semanaLabels: string[] = [];
+    let semanaData: any[] = [];
+    for (let i = 0; i < 7; i++) {
+      let totalDia = this.resetResumen();
+      const comidasDia = comidasSemana.filter((comida) => this.esMismoDiaSemana(comida.fecha, i));
+      const aguaDia = aguaSemana.filter((agua) => this.esMismoDiaSemana(agua.Fecha, i));
+      comidasDia.forEach((comida) => {
+        totalDia.calorias += comida.totales.calorias;
+        totalDia.proteina += comida.totales.proteina;
+        totalDia.carbohidratos += comida.totales.carbohidratos;
+        totalDia.grasas += comida.totales.grasas;
+        totalDia.fibra += comida.totales.fibra;
+        totalDia.azucares += comida.totales.azucares;
+        totalDia.sodio += comida.totales.sodio;
+      });
+      aguaDia.forEach((entrada) => {
+        totalDia.agua += entrada.cantidad;
+      });
+      semanaLabels.push(`Día ${i + 1}`);
+      semanaData.push(totalDia);
     }
 
-    for (const entrada of aguaSemana) {
-      this.resumenSemanal.agua += entrada.cantidad;
+    let mesLabels: string[] = [];
+    let mesData: any[] = [];
+    for (let i = 1; i <= 30; i++) {
+      let totalDia = this.resetResumen();
+      const comidasDia = comidasMes.filter((comida) => this.esMismoDiaMes(comida.fecha, i));
+      const aguaDia = aguaMes.filter((agua) => this.esMismoDiaMes(agua.Fecha, i));
+
+      comidasDia.forEach((comida) => {
+        totalDia.calorias += comida.totales.calorias;
+        totalDia.proteina += comida.totales.proteina;
+        totalDia.carbohidratos += comida.totales.carbohidratos;
+        totalDia.grasas += comida.totales.grasas;
+        totalDia.fibra += comida.totales.fibra;
+        totalDia.azucares += comida.totales.azucares;
+        totalDia.sodio += comida.totales.sodio;
+      });
+
+      aguaDia.forEach((entrada) => {
+        totalDia.agua += entrada.cantidad;
+      });
+      mesLabels.push(`Día ${i}`);
+      mesData.push(totalDia);
     }
 
-    for (const comida of comidasMes) {
-      this.resumenMensual.calorias += comida.totales.calorias;
-      this.resumenMensual.proteina += comida.totales.proteina;
-      this.resumenMensual.carbohidratos += comida.totales.carbohidratos;
-      this.resumenMensual.grasas += comida.totales.grasas;
-      this.resumenMensual.fibra += comida.totales.fibra;
-      this.resumenMensual.azucares += comida.totales.azucares;
-      this.resumenMensual.sodio += comida.totales.sodio;
-    }
+    this.barChartLabelsSemana = semanaLabels;
+    this.barChartLabelsMes = mesLabels;
 
-    for (const entrada of aguaMes) {
-      this.resumenMensual.agua += entrada.cantidad;
-    }
+    this.barChartDataSemana = {
+      labels: semanaLabels,
+      datasets: [
+        { data: semanaData.map(d => d.calorias), label: 'Calorías', backgroundColor: '#e74c3c' },
+        { data: semanaData.map(d => d.proteina), label: 'Proteína', backgroundColor: '#27ae60' },
+        { data: semanaData.map(d => d.carbohidratos), label: 'Carbohidratos', backgroundColor: '#dd9117' },
+        { data: semanaData.map(d => d.grasas), label: 'Grasas', backgroundColor: '#f1c40f' },
+        { data: semanaData.map(d => d.fibra), label: 'Fibra', backgroundColor: '#907122' },
+        { data: semanaData.map(d => d.azucares), label: 'Azúcares', backgroundColor: '#a73185' },
+        { data: semanaData.map(d => d.sodio), label: 'Sodio', backgroundColor: '#299680' },
+        { data: semanaData.map(d => d.agua), label: 'Agua (ml)', backgroundColor: '#1749d2' },
+      ]
+    };
+
+    this.barChartDataMes = {
+      labels: mesLabels,
+      datasets: [
+        { data: mesData.map(d => d.calorias), label: 'Calorías', backgroundColor: '#e74c3c' },
+        { data: mesData.map(d => d.proteina), label: 'Proteína', backgroundColor: '#27ae60' },
+        { data: mesData.map(d => d.carbohidratos), label: 'Carbohidratos', backgroundColor: '#dd9117' },
+        { data: mesData.map(d => d.grasas), label: 'Grasas', backgroundColor: '#f1c40f' },
+        { data: mesData.map(d => d.fibra), label: 'Fibra', backgroundColor: '#907122' },
+        { data: mesData.map(d => d.azucares), label: 'Azúcares', backgroundColor: '#a73185' },
+        { data: mesData.map(d => d.sodio), label: 'Sodio', backgroundColor: '#299680' },
+        { data: mesData.map(d => d.agua), label: 'Agua (ml)', backgroundColor: '#1749d2' },
+      ]
+    };
+  }
+
+  esMismoDiaSemana(fecha: string, diaSemana: number): boolean {
+    const fechaObjeto = new Date(fecha);
+    return fechaObjeto.getDay() === diaSemana;
+  }
+
+  esMismoDiaMes(fecha: string, diaMes: number): boolean {
+    const fechaObjeto = new Date(fecha);
+    return fechaObjeto.getDate() === diaMes;
   }
 }
+
