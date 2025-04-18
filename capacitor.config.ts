4b14c6b6300077e835526d3de9abb14534bd67cc
quezzaa <quezadita0409@gmail.com>
2025-04-18 11:24:43 -0600

    icono y configuracion capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.quezz.letmecook',
   appName: 'Let me cook!',
-  webDir: 'dist'
+  webDir: 'dist/browser'
 };
 
 export default config;
